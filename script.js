e2a7afcfdf7bc352d0b25bcdb35a494966f9369b
Kayk peep <kayksantana133@gmail.com>
2023-08-20 11:32:34 -0300

    att

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function inputElemento() { 
   const inputValor = document.querySelector('.js-input'); 
   let valor = Number(inputValor.value); 
-
+  const passwordMessage = document.getElementById('passwordMessage')
 
 
   if(inputValor.value === '123') { 
     window.location.href = 'twiiter.html';
-  } 
+  } else {
+    // Mostrar o P do esquecir a senha
+    passwordMessage.style.display = 'block';
+  }
 
   inputValor.value = '';
   console.log(valor)
